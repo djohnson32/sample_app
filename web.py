f813f867ebd6dbf077c24c4e5b2197ea6c5f6844
David Bradley Johnson <djohnson17@gsb.columbia.edu>
2016-10-19 10:26:46 -0400

    Get app ready for Heroku

--- web.py
+++ web.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+import giphypop
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-	#name - requests.value.get('name', 'nobody')
-	##return "Hey world!!"
+def index():	
 	return render_template('index.html')
 
-app.run(debug = True)
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+#Retrieves the user input desired search and then creates a giphy object that contains all the images from that search
+@app.route('/results')
+def results():
+	searchterm = request.values.get('name')
+	giphyobject = giphypop.Giphy()
+	results = giphyobject.search(searchterm) 
+	return render_template('results.html', results=results)
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
